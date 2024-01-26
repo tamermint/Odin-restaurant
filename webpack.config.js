fbf31963f98bfe7a8790eb2c4b32063ded1dff06
Vivek <vivek.mitra@outlook.com>
2024-01-26 18:55:01 +1100

    devtool specified and config changes made to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.css$/i, //here look for the file types if js, then \.js$
         use: ["style-loader", "css-loader"], //the loaders you installed via npm
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
