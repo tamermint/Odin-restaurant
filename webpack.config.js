51ad35f87a02e81ac4ea346b719554681a6d64ef
Vivek <vivek.mitra@outlook.com>
2024-01-26 18:48:16 +1100

    background sourced and css loaders installed via npm

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, //here look for the file types if js, then \.js$
+        use: ["style-loader", "css-loader"], //the loaders you installed via npm
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
