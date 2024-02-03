b6e75425e284ed1df654bbe15b22d3b27ec94802
Vivek <vivek.mitra@outlook.com>
2024-02-03 20:09:38 +1100

    js skeleton set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+//import other functions to retrieve content.
+
+//On page load, attach the the event listeners to the buttons
+function init() {
+  const homeBtn = document.getElementById("home-btn");
+  const aboutBtn = document.getElementById("about-btn");
+  const menuBtn = document.getElementById("menu-btn");
+}
+
+//have a main function call all these functions
+function main() {
+  init();
+  homeBtn.addEventListener("click", loadHome);
+  aboutBtn.addEventListener("click", loadAbout);
+  menuBtn.addEventListener("click", loadMenu);
+}
+
+document.onload(main());
---
+++ src/loadHome.js
@@ -0,0 +1,7 @@
+const content = document.getElementById("content");
+
+const intro = document.createElement("div");
+intro.setAttribute.add("id", "intro-page");
+intro.innerText = "Welcome to Odin's Cafe!";
+
+content.appendChild(intro);
